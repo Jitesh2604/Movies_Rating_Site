493dbcd2076c8691361720324527755789653d24
Jitesh <jiteshpal2604@gmail.com>
2024-06-24 17:54:37 +0530

    Added store

--- src/store/homeSlice.js
+++ src/store/homeSlice.js
@@ -1,22 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 export const homeSlice = createSlice({
-  name: 'home',
-  initialState: {
-    url: {},
-    genres: {}
-  },
-  reducers: {
-    getApiConfiguration: (state, action ) => {
-        state.url = action.payload;
+    name: "home",
+    initialState: {
+        url: {},
+        genres: {},
     },
-    getGenres: (state, action ) => {
-        state.genres = action.payload;
-    }
-  },
-})
+    reducers: {
+        getApiConfiguration: (state, action) => {
+            state.url = action.payload;
+        },
+        getGenres: (state, action) => {
+            state.genres = action.payload;
+        },
+    },
+});
 
 // Action creators are generated for each case reducer function
-export const { getApiConfiguration, getGenres } = homeSlice.actions
+export const { getApiConfiguration, getGenres } = homeSlice.actions;
 
-export default homeSlice.reducer 
+export default homeSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 
-import homeSlice from './homeSlice';
+import homeSlice from "./homeSlice";
 
 export const store = configureStore({
     reducer: {
-        home: homeSlice
+        home: homeSlice,
     },
 });

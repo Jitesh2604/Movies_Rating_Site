ac32dcfced6c5328183b0ea12f2b32e1c6e233cc
Jitesh <jiteshpal2604@gmail.com>
2024-06-24 17:58:43 +0530

    Added App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
-import SearchResult from "./pages/searchResult/SearchResult";
+import SearchResult from "./pages/searchResults/SearchResult";
 import Explore from "./pages/explore/Explore";
 import PageNotFound from "./pages/404/PageNotFound";
 

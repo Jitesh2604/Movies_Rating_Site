4c63bb18d9f347ca332aa0b187651e5f27d5bbbb
Jitesh <jiteshpal2604@gmail.com>
2024-06-24 18:26:21 +0530

    added moviex

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import dayjs from "dayjs";
 
-import "./style.scss";
+import "./Style.scss";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import useFetch from "../../../hooks/useFetch";
